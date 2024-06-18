c2d7533780e978808d7c7a41c50ed07cb3b6841b
skyfxllen <anton.tatarchenko3@gmail.com>
2024-06-18 22:08:39 +0700

    start page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import StartPage from "./StartPage.jsx";
+
+function App() {
+  return (
+    <div className='w-[100vw] max-w-[425px] min-h-[100%]'>
+      <StartPage />
+    </div>
+  );
+}
+
+export default App;
